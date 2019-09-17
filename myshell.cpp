f7de17ba009bfefa2769c5211c266e29f0973fea
Benjamin <bavogel@iastate.edu>
2019-09-17 10:38:49 -0500

    Added "pause" and "clear" functionality

--- myshell.cpp
+++ myshell.cpp
@@ -2,15 +2,21 @@
 // Created by Benjamin Vogel on 9/16/2019.
 //
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <iostream>
 #include <vector>
 #include <string>
+#include <limits>
 
 int main(int argc, char *argv[])
 {
     bool isRunning = true;
+
+    // Clear the screen before printing out the program
+    // TODO: Place this in a separate function
+    std::cout << "\033[2J\033[1;1H";
 
     while (isRunning)
     {
@@ -22,5 +28,20 @@ int main(int argc, char *argv[])
         {
             isRunning = false;
         }
+        else if (input.compare("pause") == 0)
+        {
+            std::cout << "Press Enter to Continue: ";
+            fgetc(stdin);
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else if (input.compare("clr") == 0)
+        {
+            // A special command to clear the screen that works for Linux and Windows
+            // Also avoids using system() calls
+            std::cout << "\033[2J\033[1;1H";
+        }
     }
+
+    // Clear the screen before exiting the program
+    std::cout << "\033[2J\033[1;1H";
 }
